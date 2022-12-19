ced44481a0edb8f68e01b68bbb4cfd46aa89ef79
Michael Locatelli <mjlocat@gmail.com>
2022-12-19 15:05:22 -0800

    Add minimum off time

--- fireplace-controller.py
+++ fireplace-controller.py
@@ -1,11 +1,12 @@
-import sys
 import os
-import json
-import time
+from datetime import datetime
 import paho.mqtt.client as mqtt
 
 MQTT_HOST = os.environ.get("MQTT_HOST")
 TEMP_THRESHOLD = float(os.environ.get("TEMP_THRESHOLD"))
+MIN_OFF_TIME = float(os.environ.get("MIN_OFF_TIME"))
+current_state = "on"
+last_state_switch = datetime.now().timestamp()
 
 def on_connect(client, userdata, flags, rc):
   print(F'Connected to {MQTT_HOST}')
@@ -15,11 +16,20 @@ def on_connect(client, userdata, flags, rc):
 def on_temperature(client, userdata, message):
   temperature = float(message.payload)
   print(F'Temperature: {temperature}')
-  state = "off"
-  if temperature < TEMP_THRESHOLD:
-    state = "on"
-  print(F'Set state to: {state}')
-  client.publish("house/fireplace/set_state", payload=state)
+  new_state = "on"
+  if temperature >= TEMP_THRESHOLD:
+    new_state = "off"
+  if new_state != current_state:
+    if new_state == "on" and datetime.now().timestamp() > last_state_switch + MIN_OFF_TIME:
+      current_state = "on"
+      last_state_switch = datetime.now().timestamp()
+      print(F'Set state to: {current_state}')
+    elif new_state == "off":
+      current_state = "off"
+      last_state_switch = datetime.now().timestamp()
+      print(F'Set state to: {current_state}')
+
+  client.publish("house/fireplace/set_state", payload=current_state)
 
 
 client = mqtt.Client(client_id="fireplace-controller")
